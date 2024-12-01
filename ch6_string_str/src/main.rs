77d39013e7f226e604a78f24ca9850e9389d1ec2
eternal <1967798998@qq.com>
2024-12-01 17:32:40 +0800

    feature:String与

--- ch6_string_str/src/main.rs
+++ ch6_string_str/src/main.rs
@@ -1,3 +1,33 @@
+struct Person<'a> {
+    name: &'a str,
+    color: String,
+    age: i32,
+}
+
+fn print(data: &str) {
+    println!("{}", data);
+}
+fn print_string_borrow(data: &String) {
+    println!("{}", data);
+}
+
 fn main() {
-    println!("Hello, world!");
+    let name = String::from("Value C++");
+    let course = "Rust".to_owned();
+    let new_name = name.replace("C++","CPP");
+    println!("{name} {course} {new_name}");
+
+    let rust = "\x52\x75\x73\x74";
+    println!("{rust}");
+
+    let color = "red".to_string();
+    let name="anna";
+    let people = Person {name, color, age: 42};
+
+    //func
+    let value = "value".to_owned();
+    print(&value);
+    print("data");
+    // print_string_borrow("borrow");
+    print_string_borrow(&value);
 }
