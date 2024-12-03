60e5f8d7af411e5e0b8c92184ec587a17a12f731
eternal <1967798998@qq.com>
2024-12-03 14:59:31 +0800

    feature:闭包类型

---
+++ ch39_closure_type/src/main.rs
@@ -0,0 +1,51 @@
+fn closure_fn<F>(func: F)
+where
+    F: Fn(),
+{
+    func();
+    func();
+}
+fn closure_fn_mut<F>(mut func: F)
+where
+    F: FnMut(),
+{
+    func();
+    func();
+}
+fn closure_fn_once<F>(func: F)
+where
+    F: FnOnce(),
+{
+    func();
+}
+
+fn main() {
+    let s1  = String::from("11111111");
+    closure_fn(|| println!("{}", s1));
+
+    let s1  = String::from("11111111");
+    closure_fn_mut(|| println!("{}", s1));
+
+    let mut s2  = String::from("22222222");
+    closure_fn_mut(|| {
+        s2.push_str("😀");
+        println!("{}", s2);
+    });
+    println!("{}", s2);
+
+    // 所有权转移
+    let s1 = String::from("11111");
+    closure_fn_once(|| println!("{}", s1));
+
+    let mut s2 = String::from("22222");
+    closure_fn_once(|| {
+        s2.push_str("😀");
+        println!("{}", s2);
+    });
+    println!("{s2}");
+
+    let s3 = " ff".to_owned();
+    closure_fn_once(move || println!("{s3}"));
+    // println!("{s3}")
+
+}
